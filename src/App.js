37a9dff14b3738d5a9bca687afd798ddd848f46e
Aman Shaikh <amanshaikh8624@gmail.com>
2024-03-05 23:32:21 -0800

    added summary

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/UI/Header';
+import Summary from './components/UI/Summary';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <div className='App-content'>
+      <Summary/>
+      </div>
+      
     </div>
   );
 }
---
+++ src/components/UI/Summary.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Summary.css"
+const Summary = () => {
+  return (
+    <div className='summary-container'>
+        <h1 className='summary-header'>Delicios food, delivered to you</h1>
+        <p className='summary-desc'>
+            Choose your favorite meal from our broad selection of available meals and enjoy a delicious lunch or dinner at home.
+            <br />
+            All our meals are cooked with high quality.
+        </p>
+    </div>
+  )
+}
+
+export default Summary
